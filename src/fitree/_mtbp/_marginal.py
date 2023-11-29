6d5c8a38a8c294aafafac8e16a81181bd9fae91c
Xiang Ge Luo <xgluo9405@gmail.com>
2023-11-29 18:32:13 +0100

    Add marginal

--- src/fitree/_mtbp/_marginal.py
+++ src/fitree/_mtbp/_marginal.py
@@ -11,6 +11,8 @@ def _g(
     theta: float | np.ndarray | mp.mpf,
     v: Subclone | Any,
     C_0: int | np.ndarray,
+    t: float | np.ndarray,
+    epsilon: float | np.ndarray | mp.mpf = 0.01,
 ) -> float | np.ndarray | mp.mpf:
     gpar_v = v.growth_params
 
@@ -19,14 +21,16 @@ def _g(
         rho = gpar_v["rho"]
 
         if gpar_v["lambda"] < 0:
-            return mp.power(phi + (1 - phi) * mp.exp(-theta), -rho * C_0)
+            return mp.power(
+                phi + (1 - phi) * mp.exp(-theta * epsilon / t), -rho * C_0 * t / epsilon
+            )
         else:
             return mp.power(1 + phi * theta, -rho * C_0)
 
     else:
         gpar_pa_v = v.parent.growth_params
         h = _h(theta, gpar_v, gpar_pa_v)
-        return _g(h, v.parent, C_0)
+        return _g(h, v.parent, C_0, t, epsilon)
 
 
 def _mcdf(
@@ -39,7 +43,7 @@ def _mcdf(
         C_tilde_v = v.get_C_tilde(t)
 
     def lp_func(theta):
-        return _g(theta, v, C_0) / theta
+        return _g(theta, v, C_0, t) / theta
 
     return mp.invertlaplace(lp_func, C_tilde_v)
 
@@ -54,6 +58,6 @@ def _mpdf(
         C_tilde_v = v.get_C_tilde(t)
 
     def lp_func(theta):
-        return _g(theta, v, C_0)
+        return _g(theta, v, C_0, t)
 
     return mp.invertlaplace(lp_func, C_tilde_v)
