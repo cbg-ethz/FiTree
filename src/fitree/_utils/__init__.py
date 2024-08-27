f033d52066fcca90631322a1be48c72b1eaeee0a
Xiang Ge Luo <xgluo9405@gmail.com>
2024-08-27 16:22:29 +0200

    Move polylog and ilp to inference

--- src/fitree/_utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""This subpackage contains utility functions implemented in JAX.
-All functions are JIT-compiled.
-"""
-
-from ._ilp import ilp_jax
-from ._polylog import polylog_jax
-
-__all__ = ["ilp_jax", "polylog_jax"]
